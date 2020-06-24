e4a62a3aa244556f671ce46fbe8b822add151233
Adrián Carmona <32549529+acarmonabelmonte@users.noreply.github.com>
2020-06-24 21:06:28 +0200

    created form component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
-  return <h1>Administrador de Pacientes</h1>;
+  return (
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
