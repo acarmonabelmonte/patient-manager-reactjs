375a8af744f0b50d66aed9814ad9dcd8bcf91c70
Adrián Carmona <32549529+acarmonabelmonte@users.noreply.github.com>
2020-06-24 22:33:40 +0200

    finish project

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from "react";
 import uuid from "uuid/dist/v4";
+import PropTypes from "prop-types";
 
 const Formulario = ({ crearCita }) => {
   // Crear State de Citas
@@ -112,6 +113,10 @@ const Formulario = ({ crearCita }) => {
       </form>
     </Fragment>
   );
+};
+
+Formulario.propTypes = {
+  crearCita: PropTypes.func.isRequired,
 };
 
 export default Formulario;
